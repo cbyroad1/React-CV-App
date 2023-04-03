4ab4c13f9365622f8fb50fcdedb96a049f752bcb
Cameron Byroad <cameronbyroad@gmail.com>
2023-04-02 19:51:50 -0500

    added header component to include title/banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MainSection from "./components/MainSection";
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MainSection />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <div id='header'>
+                <h1>CV Project</h1>
+            </div>
+        )
+    }
+}
+
+export default Header; 
